329b7fe4798bc44566844ed65283021fbdf9761c
nicolasAguero99 <nico.agueroo99@gmail.com>
2023-12-31 21:09:32 -0300

    Creatimoon project

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -9,12 +9,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.API_KEY,
-  authDomain: import.meta.env.AUTH_DOMAIN,
-  projectId: import.meta.env.PROJECT_ID,
-  storageBucket: import.meta.env.STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.MESSAGING_SENDER_ID,
-  appId: import.meta.env.APP_ID
+  apiKey: 'AIzaSyBtVtnyFGWlz1InhTHbW_bm_cRjR5n8f8g',
+  authDomain: 'parcial-firebase-40022.firebaseapp.com',
+  projectId: 'parcial-firebase-40022',
+  storageBucket: 'parcial-firebase-40022.appspot.com',
+  messagingSenderId: '591699807432',
+  appId: '1:591699807432:web:1f2f98787c98fc90e2a9fd'
 };
 
 // Tu configuración de Firebase aquí
